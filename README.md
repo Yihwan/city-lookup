985d7d385b8db822dd84b2faece667ae8cd61eca
yihwan <yihwankim@gmail.com>
2018-05-21 11:51:43 -0700

    finish basic functionality

---
+++ README.md
@@ -0,0 +1,6 @@
+# [City Lookup](https://yihwan.github.io/city-lookup/)
+How many people live here?
+
+Fetch API, RegExp, promises. Design inspired by the CVS receipt. *Guided by [Wes' JavaScript30](https://javascript30.com/)*
+
+**[Expore all JavaScript Fun projects](https://yihwan.github.io/javascript-fun/)**
--- app.js
+++ app.js
@@ -1,1 +1,53 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => {
+    data.forEach(city => {
+      cities.push(city);
+    });
+  });
+
+function findMatches(input, allData) {
+  return allData.filter(place => {
+    const regex = new RegExp(input, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches(e) {
+  const matchArray = findMatches(e.target.value, cities);
+
+  const html = matchArray.map(place => {
+    const regex = new RegExp(e.target.value, 'gi');
+
+    const cityName = place.city.replace(regex,
+      `<span class="hl">${e.target.value}</span>`);
+
+    const stateName = place.state.replace(regex,
+      `<span class="hl">${e.target.value}</span>`);
+
+    return `
+      <li>
+        <span class="name">
+          ${cityName}, ${stateName}
+        </span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
+    `;
+  }).join('');
+
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
